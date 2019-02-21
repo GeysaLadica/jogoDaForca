bb95ab03cbe8a4aa8300401f8e4ea22371d42c9c
geysa <geysa.mota01@gmail.com>
2019-02-21 19:16:35 -0300

    criar grid

---
+++ script.js
@@ -0,0 +1,79 @@
+// let senha = "Palavra";
+// let separado;
+// separado = senha.split("");
+// console.log(separado); 
+// let letraUser = prompt("Digite uma letra");
+// let tamanho;
+
+
+// const verificar = function (){
+//     for (let i = 0; i < separado.length; i++){
+//         if(separado[i] === letraUser){
+//             console.log("existe");
+//             // return;
+//         }
+//         else{
+//             console.log("nao existe");
+//             // return;
+//         }
+//     }
+// }
+// const compararLetra = document.querySelector('p');
+// compararLetra.innerHTML = verificar();
+//ate aqui funciona :)
+
+// let senha = "Palavra";
+// let separado;
+// separado = senha.split("");
+// console.log(separado); 
+// let letraUser = prompt("Digite uma letra");
+// let tamanho;
+// let meAltere = document.querySelector("#exibicao");
+
+
+//     for (let i = 0; i < separado.length; i++){
+//         if(separado[i] === letraUser){
+//             // const verificar = function(){
+//             //     return;
+//             console.log("existe");
+//             meAltere.innerHTML = "existe";
+ 
+//         }
+//         else{
+//             console.log("nao existe");
+//             meAltere.innerHTML = "nao existe";
+     
+//         }
+//     }
+// const compararLetra = document.querySelector('p');
+// compararLetra.innerHTML = verificar();
+
+
+// let senha = "Palavra";
+// let separado;
+// separado = senha.split("");
+// console.log(separado); 
+// let letraUser = prompt("Digite uma letra");
+// let tamanho;
+// let meAltere = document.querySelector("#exibicao");
+
+
+//     for (let i = 0; i < separado.length; i++){
+//         const verificarA = function(){
+//             if(separado[i] === letraUser){
+//                 // const verificar = function(){
+//                 //     return;
+//                 console.log("existe");
+//                 meAltere.innerHTML = "existe";
+    
+//             }
+//             else{
+//                 console.log("nao existe");
+//                 meAltere.innerHTML = "nao existe";
+        
+//             }
+//         }
+//     }
+
+
+
